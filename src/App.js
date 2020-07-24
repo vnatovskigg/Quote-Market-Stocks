39b1d7bbf48fb6e5a411a71817556fab8e5e8107
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-07-24 18:53:43 +0300

    feat:breaking down into smaller components and implementing css modules

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./styles/App.css";
-import Nav from "./Nav";
+import Nav from "./components/navigation";
 import About from "./About";
 import Contact from "./Contact";
 import Screen from "./Stock-screen";
---
+++ src/components/logo/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./index.module.css";
+
+const Logo = () => {
+  return <h3 className={styles.logo}>Logo</h3>;
+};
+
+export default Logo;
