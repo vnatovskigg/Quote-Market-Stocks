1d0401070313df9070132977318f93e0679ba0eb
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-08-11 17:48:17 +0300

    feat: update

---
+++ src/components/page-wrapper/index.js
@@ -0,0 +1,25 @@
+<<<<<<< Updated upstream
+=======
+import React from "react";
+import Nav from "../navigation";
+import Screen from "../stock-screen";
+import Footer from "../footer";
+import StockTV from "../screener-widget";
+import styles from "./index.module.css";
+
+function PageWrapper(props) {
+  return (
+    <div className={styles.App}>
+      <div className={styles.main}>
+        <Nav />
+        <StockTV />
+        <Screen />
+        <div className={styles.content}>{props.children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default PageWrapper;
+>>>>>>> Stashed changes
