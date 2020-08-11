fd284125e3bf88a360da2f15597edb3a472baf40
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-08-11 11:35:06 +0300

    feat: refacturing components for optimizations

--- src/App.js
+++ src/App.js
@@ -1,31 +1,104 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import "./styles/App.css";
-import Nav from "./components/navigation";
-import About from "./components/about";
-import Contact from "./components/contact";
-import Screen from "./components/stock-screen";
-import Footer from "./components/footer";
-import StockTV from "./components/screener-widget";
+import React, { useState, useEffect } from "react";
+import UserContext from "./Context";
+import getCookie from "./services/cookie";
 
-function App() {
+const App = (props) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const logIn = (user) => {
+    setUser({
+      ...user,
+      loggedIn: true,
+    });
+  };
+
+  const logOut = () => {
+    document.cookie = "x-auth-token= ; expires = Thu, 01 Jan 1970 00:00:00 GMT";
+    setUser({
+      loggedIn: false,
+    });
+  };
+
+  useEffect(() => {
+    const token = getCookie("x-auth-token");
+
+    if (!token) {
+      logOut();
+      setLoading(false);
+      return;
+    }
+
+    fetch("http://localhost:9999/api/user/verify", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    })
+      .then((promise) => {
+        return promise.json();
+      })
+      .then((res) => {
+        if (res.status) {
+          logIn({
+            username: res.user.username,
+            id: res.user._id,
+          });
+        } else {
+          logOut();
+        }
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <div>Loading....</div>;
+  }
+
   return (
-    <Router>
-      <div className="App">
-        <div className="main">
-          <Nav />
-          <StockTV />
-          {/* <Screen /> */}
-          <Switch>
-            {/* <Route path="/" exact component={Home} /> */}
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-          </Switch>
-        </div>
-        <Footer />
-      </div>
-    </Router>
+    <UserContext.Provider
+      value={{
+        user,
+        logIn,
+        logOut,
+      }}
+    >
+      {props.children}
+    </UserContext.Provider>
   );
-}
+};
 
 export default App;
+
+// import React from "react";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+// import "./styles/App.css";
+// import Nav from "./components/navigation";
+// import About from "./components/about";
+// import Contact from "./components/contact";
+// import Screen from "./components/stock-screen";
+// import Footer from "./components/footer";
+// import StockTV from "./components/screener-widget";
+
+// function App() {
+//   return (
+//     <Router>
+//       <div className="App">
+//         <div className="main">
+//           <Nav />
+//           <StockTV />
+//           {/* <Screen /> */}
+//           <Switch>
+//             {/* <Route path="/" exact component={Home} /> */}
+//             <Route path="/about" component={About} />
+//             <Route path="/contact" component={Contact} />
+//           </Switch>
+//         </div>
+//         <Footer />
+//       </div>
+//     </Router>
+//   );
+// }
+
+// export default App;
---
+++ src/navigation.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Portfolio from "./pages/portfolio";
+import Profile from "./pages/profile";
+import Register from "./pages/register";
+
+const Navigation = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/register" compoenent={Register} />
+        <Route path="/login" component={Login} />
+        <Route path="/profile/:id" component={Profile} />
+        <Route path="/portfolio" component={Portfolio} />
+        <Route path="/about" component={About} />
+        <Route path="/contact" component={Contact} />
+        <Route component={ErrorPage} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Navigation;
