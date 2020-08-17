a6b9b6611088b3914228f01bf5ce6c840f816fce
vnatovskigg <49907900+vnatovskigg@users.noreply.github.com>
2020-08-17 14:19:16 +0300

    feat: register page completed

--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,89 +1,89 @@
-// import React, { useState, useContext } from "react";
-// import { useForm } from "react-hook-form";
-// import styles from "./index.module.css";
-// import PageWrapper from "../../components/page-wrapper";
-// import authenticate from "../../utils/authenticate";
-// import UserContext from "../../Context";
-// import { useHistory } from "react-router-dom";
+import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import styles from "./index.module.css";
+import PageWrapper from "../../components/page-wrapper";
+import ContentWrapper from "../../components/content-wrapper";
+import authenticate from "../../services/authenticate";
+import UserContext from "../../Context";
+import { useHistory } from "react-router-dom";
 
-// const Register = () => {
-//   const { register, handleSubmit, errors, reset } = useForm({
-//     mode: "onSubmit",
-//     reValidateMode: "onSubmit",
-//   });
-//   const context = useContext(UserContext);
-//   const history = useHistory();
+const Register = () => {
+  const { register, handleSubmit, errors, reset, getValues } = useForm();
+  const context = useContext(UserContext);
+  const history = useHistory();
 
-//   const onSubmit = async (e) => {
-//     e.preventDefault();
+  const onSubmit = async (data) => {
+    const { username, password } = data;
+    console.log(username, password);
 
-//     await authenticate(
-//       "http://localhost:8888/api/user/register",
-//       {
-//         username,
-//         password,
-//       },
-//       (user) => {
-//         console.log("Success");
-//         context.logIn(user);
-//         history.push("/");
-//       },
-//       (e) => {
-//         console.log("Error", e);
-//       }
-//     );
-//   };
+    await authenticate(
+      "http://localhost:8888/api/user/register",
+      {
+        username,
+        password,
+      },
+      (user) => {
+        console.log("Success");
+        context.logIn(user);
+        history.push("/");
+      },
+      (e) => {
+        console.log("Error", e);
+      }
+    );
+  };
 
-//   return (
-//     <PageWrapper>
-//       <div className={styles.wrapper}>
-//         <div className={styles.heading}>
-//           <h1>Register</h1>
-//         </div>
-//         <div className={styles.content}>
-//           <form
-//             className={styles["contact-form"]}
-//             onSubmit={handleSubmit(onSubmit)}
-//           >
-//             <input
-//               type="text"
-//               placeholder="Username"
-//               name="username"
-//               ref={register({ required: true, maxLength: 80 })}
-//             />
+  return (
+    <PageWrapper>
+      <ContentWrapper title="Register">
+        <form
+          className={styles["register-form"]}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            type="text"
+            placeholder="Username"
+            name="username"
+            ref={register({ required: true, maxLength: 80 })}
+          />
 
-//             {errors.name && <span>Username is required</span>}
+          {errors.username && <span>Required field</span>}
 
-//             <input
-//               type="text"
-//               placeholder="Password"
-//               name="password"
-//               ref={register({ required: true, minLength: 6 })}
-//             />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            ref={register({ required: true, minLength: 6 })}
+          />
 
-//             {errors.email && (
-//               <span>Password must be at least 6 characters long</span>
-//             )}
+          {errors.password && (
+            <span>Password must be at least 6 characters long</span>
+          )}
 
-//             <input
-//               type="text"
-//               placeholder="Repeat Password"
-//               name="rePassword"
-//               ref={register({ required: true, minLength: 6 })}
-//             />
+          <input
+            type="password"
+            placeholder="Repeat Password"
+            name="rePassword"
+            ref={register({
+              required: true,
+              minLength: 6,
+              validate: (value) => value === getValues("password"),
+            })}
+          />
 
-//             <div className={styles["submit-div"]}>
-//               <input
-//                 className={styles["form-submit"]}
-//                 type="submit"
-//                 value={"Register"}
-//               />
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </PageWrapper>
-//   );
-// };
+          {errors.rePassword && <span>Passwords must match</span>}
 
-// export default Register;
+          <div className={styles["submit-div"]}>
+            <input
+              className={styles["form-submit"]}
+              type="submit"
+              value={"Register"}
+            />
+          </div>
+        </form>
+      </ContentWrapper>
+    </PageWrapper>
+  );
+};
+
+export default Register;
